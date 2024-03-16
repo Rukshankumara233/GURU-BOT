b7a05d63fbd080a80c15bf22d3babf6119b8a6bc
Rukshankumara233 <160301902+Rukshankumara233@users.noreply.github.com>
2024-03-16 21:00:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94774589636', 'Rukshan', true],
+  ['94774589636', 'Sk', true], 
   [''] 
 ] //Number of owners
 
